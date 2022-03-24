ef5439a6a9c532b8c5c1df45afc3ccf4fa68e6fa
Ahmad Syamim <ahmad.syamim@gmail.com>
2022-03-24 22:29:28 +0900

    init commit

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('mosque::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('mosque.name') !!}
+    </p>
+@endsection
